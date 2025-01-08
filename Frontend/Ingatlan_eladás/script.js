958e383d63759d2af148b99dfe39db37621ad6d8
Chen520-it <78354498+Chen520-it@users.noreply.github.com>
2025-01-08 21:57:17 +0100

    20250108

--- Frontend/Ingatlan_eladás/script.js
+++ Frontend/Ingatlan_eladás/script.js
@@ -1,4 +1,4 @@
-const ajandekok = [
+const lakasok= [
   { img: "https://ingatlan5.hu/public/storage/Ingatlanado2.jpg", value: 0 },
   { img: "https://ingatlan5.hu/public/storage/Ingatlanado2.jpg", value: 0 },
   { img: "https://ingatlan5.hu/public/storage/Ingatlanado2.jpg", value: 0 },
@@ -13,44 +13,50 @@ const ajandekok = [
   { img: "https://ingatlan5.hu/public/storage/Ingatlanado2.jpg", value: 0 }
 
 ];
-
+//获取html的元素
 const container = document.getElementById("box-container");
 const legtobb = document.getElementById("legtobb");
 
 // 动态生成方块
-ajandekok.forEach((item, index) => {
+lakasok.forEach((item, index) => {
   const box = document.createElement("div");
   box.className = "box";
-  // 标记方块的索引
+  // 设计方块属性
   box.setAttribute("data-index", index); 
   box.setAttribute("data-value", item.value);
 
+  //让每个方块都添加图片 并且标记成房子几号
   const img = document.createElement("img");
   img.src = item.img;
-  img.alt = `Ajándék ${index + 1}`;
+  img.alt = `Lakás ${index+1}`;
 
+  //创建显示票数的文本
   const span = document.createElement("span");
   span.textContent = `${item.value} szavazás`;
 
+  //创建按钮
+  const button = document.createElement("button");
+  button.textContent = "Érdeklődés";
+  button.className = "erdeklodes"
+
+  //将图片和文字添加到方块里面
   box.appendChild(img);
   box.appendChild(span);
+  box.appendChild(button);
 
-  const buttonContainer =
+  //点击事件，增加票数然后更新票数
+  box.addEventListener("click", () => {
+  let szavazas = parseInt(box.getAttribute("data-value"), 10);
+  szavazas += 1;
+  box.setAttribute("data-value", szavazas);
+  span.textContent = `${szavazas} szavazás`; 
+  //更新全局的数据
+  lakasok[index].value=szavazas;
 
-
-
-      box.addEventListener("click", () => {
-      let szavazas = parseInt(box.getAttribute("data-value"), 10);
-      szavazas += 1;
-      box.setAttribute("data-value", szavazas);
-      span.textContent = `${szavazas} szavazás`; 
-
-      ajandekok[index].value=szavazas;
-
-      // 更新票数
-      frisites();
+  // 更新票数排名
+  frisites();
   });
-
+  //将方块添加到box里
   container.appendChild(box);
 });
 
@@ -60,7 +66,7 @@ function frisites() {
   let maxIndex = -1;
 
   // 票数最高的显示出来
-  ajandekok.forEach((item, index) => {
+  lakasok.forEach((item, index) => {
       const value=item.value;
 
       if (value > maxValue) {
@@ -72,7 +78,8 @@ function frisites() {
   // 更新结果
   if (maxValue > 0) {
       legtobb.textContent = `A legtöbb szavazatot kapott: ${maxIndex + 1},   ${maxValue}db szavazás.`;
-  } else {
+  } 
+  else {
       legtobb.textContent = "Jelenleg nincs szavazat.";
   }
 }
