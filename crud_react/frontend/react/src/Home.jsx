a833449a35cbafa34c006163447b9e6244f9bdd3
Chen520-it <78354498+Chen520-it@users.noreply.github.com>
2025-01-31 13:21:29 +0100

    20250131

---
+++ crud_react/frontend/react/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {link} from "reactrouter-dom";
+
+const Home  = ()=>{
+    return(
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Szia</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
