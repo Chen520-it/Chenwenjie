20d29418101f0b2669669ddf8d678dc069d9fb65
Chen520-it <78354498+Chen520-it@users.noreply.github.com>
2025-03-03 00:05:33 +0100

    forras backend

--- Backend/forras/backend/server.js
+++ Backend/forras/backend/server.js
@@ -9,7 +9,17 @@ const connection = mysql.createConnection({
     password:"",
     database:"fogado"
 
-})
+});
+
+connection.connect(err =>{
+    if(err){
+        console.error('Adatbazis nincs osszekotve:', err);
+
+    }
+    else{
+        console.log('Sikeres');
+    }
+});
 
 app.use(cors());
 
@@ -24,12 +34,25 @@ app.listen(3000,() =>{
 app.get("/szobak", (req, res)=>{
     const db = "Select * FROM szobak";
     connection.query(db, (err, result)=>{
-        if(err) return res.json(err);
+        if(err) return res.json({error: err.message});
         return res.json(result);
     })
 });
 
-app.get("/foglalasok", (req, res) =>{
-    const db = "Select FROM foglalasok"
+app.get("/szoba/:szazon", (req, res) =>{
+    db.query( "Select * FROM szobak WHERE szazon = ?",[szazon], (err, result)=>{
+        if(err) return res.status(500).json({error: err.message});
+        res.json(result[0]);
+    })
     
-})
+});
+
+app.get("/foglaltsag/:szazon", (req, res) =>{
+    const { szazon }= req.params;
+    const sql = "Select szobak.szazon, szobak.sznev, vendegek.vnev, foglalasok.erk, foglalsok.tav FROM foglalasok INNER JOIN szobak ON foglalasok.szoba = szobak.szazon INNER JOIN vendekek ON foglalasok.vendeg = vendegek.vsorsz WHERE szobak.szazon = ? ORDER BY vendegek.vnev"
+    db.query(sql,[szazon], (err, result)=>{
+        if(err) return res.status(500).json({error: err.message});
+        res.json(result[0]); 
+    })
+});
+    
